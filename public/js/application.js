065623f7615d71d701b2e1fe6a8de36d0c8c42ca
ayoa77 <ayodeleamadi@gmail.com>
2017-02-10 22:18:19 +0800

    updating for deployment

--- public/js/application.js
+++ public/js/application.js
@@ -1,18 +1,21 @@
-document.ready(function(){
+$(document).ready(function(){
   // $('#main-heading').animate(){
 
 
   // }
   $('#url-form').submit(function(event){
     event.preventDefault();
+    
     $.ajax({
-      url: '/url',
-      method: 'POST',
+      type: 'POST',
+      url: '/urls',
       data: $(this).serialize(),
-      dataType: 'json'
-    },
-      success: function(data){console.log(data)}
+      dataType: 'json',
+      success: function(data){
+        console.log(data)
+
       }
+    })
+
     });
   });
-});
