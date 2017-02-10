a1deabe9e7b546837c5e9685638ce25fb5036ec2
ayoa77 <ayodeleamadi@gmail.com>
2017-02-10 22:32:34 +0800

    fix/update migration files

--- db/migrate/20170210222952_create_urls.rb
+++ db/migrate/20170210222952_create_urls.rb
@@ -1,9 +1,9 @@
-class AddColumnToUrl < ActiveRecord::Migration
+class CreateUrls < ActiveRecord::Migration
 	def change
 		create_table :urls do |t|
 		t.string :long_url
 		t.string :short_url
-    t.integer :click_count, :default=> 0
+		t.integer :click_count, :default=> 0
 		t.timestamps
 	end
 	end
